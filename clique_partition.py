f838bc9587fc1b2ff15e642dd98aef12a3942beb
mip182 <nikitakozlov39@gmail.com>
2024-05-11 00:30:02 +0300

    use knn from sklearn in clique_partition + use find_cliques to find cliques

--- clique_partition.py
+++ clique_partition.py
@@ -1,64 +1,72 @@
+#!/Users/mip182/opt/anaconda3/bin/python3
+
 import numpy as np
-from scipy.spatial.distance import pdist, squareform
 import networkx as nx
-from itertools import combinations
+from itertools import combinations, chain
+from sklearn.neighbors import NearestNeighbors
 
 
 class SimplexBuilder:
-    def __init__(self, embeddings, k, eps, metric='euclidean'):
+    def __init__(self, embeddings, k, t, metric='euclidean'):
         """
-        Initializes the SimplexBuilder with embeddings, simplex size, distance threshold, and metric.
+        Initializes the SimplexBuilder with embeddings, simplex size, number of neighbors, and metric.
         :param embeddings: np.array - An array of embeddings.
-        :param k: int - The size of the simplices (cliques) to find.
-        :param eps: float - The maximum distance between connected embeddings.
+        :param k: int - The maximum size of the simplices (cliques) to find.
+        :param t: int - The number of nearest neighbors to consider for building the graph.
         :param metric: str - The metric to use for distance calculation, default is 'euclidean'.
         """
         self.embeddings = embeddings
         self.k = k
-        self.eps = eps
+        self.t = t
         self.metric = metric
         self.graph = nx.Graph()
 
-    def build_graph(self):
+    def build_knn_graph(self):
         """
-        Builds a graph where each node represents an embedding and edges connect nodes
-        that are within eps distance of each other according to the specified metric.
+        Builds a KNN graph using sklearn's NearestNeighbors, then symmetrizes the adjacency matrix.
         """
-        distances = squareform(pdist(self.embeddings, metric=self.metric))
-        for i in range(len(distances)):
-            for j in range(i + 1, len(distances)):
-                if distances[i][j] <= self.eps:
+        knn = NearestNeighbors(n_neighbors=self.t, metric=self.metric)
+        knn.fit(self.embeddings)
+        distances, indices = knn.kneighbors(self.embeddings)
+
+        n_samples = self.embeddings.shape[0]
+        adj_matrix = np.zeros((n_samples, n_samples), dtype=int)
+
+        for i, neighbors in enumerate(indices):
+            for neighbor in neighbors:
+                adj_matrix[i, neighbor] = 1
+
+        sym_adj_matrix = adj_matrix + adj_matrix.T
+        sym_adj_matrix[sym_adj_matrix > 1] = 1
+
+        self.graph.add_nodes_from(range(n_samples))
+        for i in range(n_samples):
+            for j in range(i + 1, n_samples):
+                if sym_adj_matrix[i, j] == 1:
                     self.graph.add_edge(i, j)
 
     def find_cliques(self):
         """
-        Finds cliques of size k, iteratively removing vertices once they are part of a found clique or alone.
+        Finds cliques of size up to k using the Bron-Kerbosch algorithm provided by networkx find_cliques.
+        Adds all subsets of size <= k from each clique to the cliques_of_partition and sorts them by size.
         """
         cliques_of_partition = []
-        G = self.graph.copy()
-        nodes = list(G.nodes())
-        i = 0
-        while i < len(nodes):
-            found = False
-            current_node = nodes[i]
-            for clique in combinations(set(G.neighbors(current_node)) | {current_node}, self.k):
-                if len(clique) == self.k and G.subgraph(clique).number_of_edges() == self.k * (self.k - 1) / 2:
-                    cliques_of_partition.append(list(clique))
-                    G.remove_nodes_from(clique)
-                    nodes = [node for node in nodes if node not in clique]
-                    found = True
-                    break
-            if not found:
-                G.remove_node(current_node)
-                nodes.remove(current_node)
-            i = 0
+        all_cliques = list(nx.find_cliques(self.graph))
+
+        for clique in all_cliques:
+            for r in range(1, min(len(clique), self.k) + 1):
+                cliques_of_partition.extend(list(combinations(clique, r)))
+
+        cliques_of_partition = list(set(map(tuple, cliques_of_partition)))
+        cliques_of_partition.sort(key=len)
+
         return cliques_of_partition
 
 
-# Example usage
 if __name__ == "__main__":
+    # Example usage
     embeddings = np.random.rand(10, 5)
-    simplex_builder = SimplexBuilder(embeddings, k=3, eps=0.5)
-    simplex_builder.build_graph()
+    simplex_builder = SimplexBuilder(embeddings, k=3, t=4)
+    simplex_builder.build_knn_graph()
     cliques = simplex_builder.find_cliques()
-    print("Found cliques of size k:", cliques)
+    print("Found cliques of size <= k:", cliques)
